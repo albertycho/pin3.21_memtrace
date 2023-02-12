f63fb7fa0e447e07b906245d4b4331a625f57f4a
albertycho90 <albertycho90@gmail.com>
2023-02-12 12:35:21 -0500

    cleanup code up to this point

--- pin321/source/tools/Memory/memtrace.cpp
+++ pin321/source/tools/Memory/memtrace.cpp
@@ -20,40 +20,10 @@ FILE * trace;
 KNOB<std::string> KnobOutputFile(KNOB_MODE_WRITEONCE, "pintool", "o", "l3trace.out", "specify output file name");
 
 uint64_t ins_count=0;
-uint64_t ins_count_2=0;
 uint64_t memref_single_count=0;
 uint64_t memref_multi_count=0;
 uint64_t num_maccess=0;
 
-namespace ITLB
-{
-// instruction TLB: 4 kB pages, 32 entries, fully associative
-const UINT32 lineSize                          = 4 * KILO;
-const UINT32 cacheSize                         = 32 * lineSize;
-const UINT32 associativity                     = 32;
-const CACHE_ALLOC::STORE_ALLOCATION allocation = CACHE_ALLOC::STORE_ALLOCATE;
-
-const UINT32 max_sets          = cacheSize / (lineSize * associativity);
-const UINT32 max_associativity = associativity;
-
-typedef CACHE_ROUND_ROBIN(max_sets, max_associativity, allocation) CACHE;
-} // namespace ITLB
-static ITLB::CACHE itlb("ITLB", ITLB::cacheSize, ITLB::lineSize, ITLB::associativity);
-
-namespace DTLB
-{
-// data TLB: 4 kB pages, 32 entries, fully associative
-const UINT32 lineSize                          = 4 * KILO;
-const UINT32 cacheSize                         = 32 * lineSize;
-const UINT32 associativity                     = 32;
-const CACHE_ALLOC::STORE_ALLOCATION allocation = CACHE_ALLOC::STORE_ALLOCATE;
-
-const UINT32 max_sets          = cacheSize / (lineSize * associativity);
-const UINT32 max_associativity = associativity;
-
-typedef CACHE_ROUND_ROBIN(max_sets, max_associativity, allocation) CACHE;
-} // namespace DTLB
-static DTLB::CACHE dtlb("DTLB", DTLB::cacheSize, DTLB::lineSize, DTLB::associativity);
 
 namespace IL1
 {
@@ -70,34 +40,6 @@ typedef CACHE_ROUND_ROBIN(max_sets, max_associativity, allocation) CACHE;
 } // namespace IL1
 static IL1::CACHE il1("L1 Instruction Cache", IL1::cacheSize, IL1::lineSize, IL1::associativity);
 
-namespace DL1
-{
-// 1st level data cache: 32 kB, 32 B lines, 32-way associative
-const UINT32 cacheSize                         = 32 * KILO;
-const UINT32 lineSize                          = 32;
-const UINT32 associativity                     = 32;
-const CACHE_ALLOC::STORE_ALLOCATION allocation = CACHE_ALLOC::STORE_NO_ALLOCATE;
-
-const UINT32 max_sets          = cacheSize / (lineSize * associativity);
-const UINT32 max_associativity = associativity;
-
-typedef CACHE_ROUND_ROBIN(max_sets, max_associativity, allocation) CACHE;
-} // namespace DL1
-static DL1::CACHE dl1("L1 Data Cache", DL1::cacheSize, DL1::lineSize, DL1::associativity);
-
-namespace UL2
-{
-// 2nd level unified cache: 2 MB, 64 B lines, direct mapped
-const UINT32 cacheSize                         = 2 * MEGA;
-const UINT32 lineSize                          = 64;
-const UINT32 associativity                     = 1;
-const CACHE_ALLOC::STORE_ALLOCATION allocation = CACHE_ALLOC::STORE_ALLOCATE;
-
-const UINT32 max_sets = cacheSize / (lineSize * associativity);
-
-typedef CACHE_DIRECT_MAPPED(max_sets, allocation) CACHE;
-} // namespace UL2
-static UL2::CACHE ul2("L2 Unified Cache", UL2::cacheSize, UL2::lineSize, UL2::associativity);
 
 namespace UL3
 {
@@ -115,19 +57,7 @@ static UL3::CACHE ul3("L3 Unified Cache", UL3::cacheSize, UL3::lineSize, UL3::as
 
 static VOID Fini(int code, VOID* v)
 {
-    //std::cerr << itlb;
-    //std::cerr << dtlb;
-    //std::cerr << il1;
-    //std::cerr << dl1;
-    //std::cerr << ul2;
-    //std::cerr << ul3;
 
-    ////std::cout << itlb;
-    ////std::cout << dtlb;
-    //std::cout << il1;
-    //std::cout << dl1;
-    //std::cout << ul2;
-    //std::cout << ul3;
     std::cout << "num mem accesses: "<<num_maccess <<std::endl;
     std::cout << "Fini finished"<<std::endl;
 }
@@ -141,41 +71,30 @@ static VOID Ul3Access(ADDRINT addr, UINT32 size, CACHE_BASE::ACCESS_TYPE accessT
     }
 }
 
-//temporary comment out while not used
-//static VOID Ul2Access(ADDRINT addr, UINT32 size, CACHE_BASE::ACCESS_TYPE accessType)
-//{
-//    // second level unified cache
-//    const BOOL ul2Hit = ul2.Access(addr, size, accessType);
-//
-//    // third level unified cache
-//    //if (!ul2Hit) ul3.Access(addr, size, accessType);
-//    if (!ul2Hit) Ul3Access(addr, size, accessType);
-//}
+
 
 static VOID InsRef(ADDRINT addr)
 {
-    ins_count_2++;
-    if(ins_count_2 % 10000==0){
-    std::cout<<"ins_count_2: "<<ins_count_2/10000<<"*10k"<<std::endl;
+    ins_count++;
+    if(ins_count % 10000==0){
+    std::cout<<"ins_count: "<<ins_count/1000<<" K"<<std::endl;
     }
 
     const UINT32 size                        = 1; // assuming access does not cross cache lines
     const CACHE_BASE::ACCESS_TYPE accessType = CACHE_BASE::ACCESS_TYPE_LOAD;
 
-    //// ITLB
-    //itlb.AccessSingleLine(addr, accessType);
+    //// first level I-cache (Got rid of l1/l2 for data cache, keeping IL1
+    const BOOL il1Hit = il1.AccessSingleLine(addr, accessType);
+    if (!il1Hit) Ul3Access(addr, size, accessType);
 
-    //// first level I-cache
-    //const BOOL il1Hit = il1.AccessSingleLine(addr, accessType);
-
-    //// second level unified Cache
-    //if (!il1Hit) Ul2Access(addr, size, accessType);
-    //Ul2Access(addr, size, accessType);
-    Ul3Access(addr, size, accessType);
 }
 
 static VOID MemRefMulti(ADDRINT addr, UINT32 size, CACHE_BASE::ACCESS_TYPE accessType)
 {
+    //TODO figure out how to handle memref_multi
+    Ul3Access(addr, size, accessType);
+    return;
+
     //memref_multi_count++;
     //if(memref_multi_count % 10000==0){
     //std::cout<<"memref_multi_count : "<<memref_multi_count/10000<<"*10k"<<std::endl;
@@ -188,60 +107,31 @@ static VOID MemRefMulti(ADDRINT addr, UINT32 size, CACHE_BASE::ACCESS_TYPE acces
     //for(UINT32 i=0;i<size;i++){
     //  ADDRINT addr_i = addr + i*64;
     //  fprintf(trace, "%p\n", (void *)addr_i);
-
     //}
     //return;
     ////above is test code. TODO REMOVE
-    
-    
-    //std::cout<<"in MemRefMulti, size: "<<size<<std::endl;
-    // DTLB : acho: idk if we need this for our purpose. TODO commnet out and see
-    //dtlb.AccessSingleLine(addr, CACHE_BASE::ACCESS_TYPE_LOAD);
+  
 
-    //// first level D-cache
-    //const BOOL dl1Hit = dl1.Access(addr, size, accessType);
-
-    //// second level unified Cache
-    //if (!dl1Hit) Ul2Access(addr, size, accessType);
-
-    //TODO figure out how to handle memref_multi
-    //Ul2Access(addr, size, accessType);
-    Ul3Access(addr, size, accessType);
 
     
 }
 
 static VOID MemRefSingle(ADDRINT addr, UINT32 size, CACHE_BASE::ACCESS_TYPE accessType)
 {
+    Ul3Access(addr, size, accessType);
+    return;
+    //DBG counter
     //memref_single_count++;
-    //if(memref_single_count % 10000==0){
-    //std::cout<<"memref_single_count : "<<memref_single_count/10000<<"*10k"<<std::endl;
-    //}
-
-
+    //if(memref_single_count % 10000==0) {std::cout<<"memref_single_count : "<<memref_single_count/10000<<"*10k"<<std::endl;}
+    //DBG for all accesses
     //fprintf(trace, "%p\n", (void *)addr);
     //return;
     
-    ////above is test code. TODO REMOVE
-
-    // DTLB
-    //dtlb.AccessSingleLine(addr, CACHE_BASE::ACCESS_TYPE_LOAD);
-
-    // first level D-cache
-    //const BOOL dl1Hit = dl1.AccessSingleLine(addr, accessType);
-
-    //// second level unified Cache
-    //if (!dl1Hit) Ul2Access(addr, size, accessType);
-    //Ul2Access(addr, size, accessType);
-    Ul3Access(addr, size, accessType);
 
 }
 static VOID Instruction(INS ins, VOID* v)
 {
-    ins_count++;
-    if(ins_count % 10000==0){
-    std::cout<<"ins_count: "<<ins_count/10000<<"*10k"<<std::endl;
-    }
+
     // all instruction fetches access I-cache
     INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)InsRef, IARG_INST_PTR, IARG_END);
 

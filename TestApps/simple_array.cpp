e89aecbd688c3abd5379fd2562439c5d0c5dcb74
Albert Y Cho <albertycho90@gmail.com>
2023-05-08 18:05:25 -0400

    simplearray with counter

--- TestApps/simple_array.cpp
+++ TestApps/simple_array.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <fstream>
 
-#define ARR_SIZE 1024*1024
+#define ARR_SIZE 1024*1024*256
 //#define ARR_SIZE 1048576
 
 int main(){
@@ -13,6 +13,12 @@ int main(){
         arr[i]=i;
     }
     int sum=0;
+		uint64_t dummyval;
+		__asm__ __volatile__(
+            "xchg %%rbx, %%rbx;"
+            ::"b"((uint64_t)(&dummyval)),"c"((uint64_t)0x5) //clobbered registers
+        );
+
     for (int i=0; i<ARR_SIZE;i++){
         sum+=arr[i];
     }

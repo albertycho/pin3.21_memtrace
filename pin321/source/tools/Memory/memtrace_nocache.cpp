52970fef0636bc85de78eb031d38430666887b34
Albert <albertycho90@gmail.com>
2023-07-18 14:29:54 +0900

    memtrace no cache first ver

---
+++ pin321/source/tools/Memory/memtrace_nocache.cpp
@@ -0,0 +1,477 @@
+/*
+ * Copyright (C) 2004-2021 Intel Corporation.
+ * SPDX-License-Identifier: MIT
+ */
+
+/*! @file
+ *  This file contains an ISA-portable PIN tool for functional simulation of
+ *  instruction+data TLB+cache hierarchies
+ */
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include "pin.H"
+#include <fstream>
+#include <stdlib.h>
+#include <string.h>
+
+typedef UINT32 CACHE_STATS; // type of cache hit/miss counters
+
+#include "pin_cache_ac.H"
+
+#define MAX_THREADS 64
+
+#define PHASE_INTERVAL 1000000000 //1 Billoion (insts)
+
+#define MT_START 5000000000
+#define MT_END   6000000000
+
+BOOL tr_done[MAX_THREADS]={};
+
+
+
+//FILE * trace_sancheck;
+FILE * trace[MAX_THREADS];
+//FILE * ins_trace[MAX_THREADS];
+//KNOB<std::string> KnobOutputFile(KNOB_MODE_WRITEONCE, "pintool", "o", "memtrace.out", "specify output file name");
+KNOB< BOOL > KnobStartFF(KNOB_MODE_WRITEONCE, "pintool", "startFF", "0", "no trace until ROI start indicated");
+KNOB<UINT64> KnobSkipInstructions(KNOB_MODE_WRITEONCE, "pintool", "s", "0", 
+        "How many instructions to skip before tracing begins");
+
+KNOB<UINT64> KnobTraceInstructions(KNOB_MODE_WRITEONCE, "pintool", "t", "1000000", 
+        "How many instructions to trace");
+
+BOOL     inROI[MAX_THREADS];
+BOOL	 inROI_master;
+uint64_t ins_count[MAX_THREADS] = {};
+uint64_t memref_single_count=0;
+uint64_t memref_multi_count=0;
+uint64_t num_maccess[MAX_THREADS] = {};
+
+uint64_t champsim_skipins=0;
+uint64_t champsim_traceins=2000000000; //2B inst by default
+uint64_t champsim_tracedoneins=2000000000; //skipins+traceins
+bool champsim_trace_done=false;
+
+//intermediate data structure to batch write to file
+#define TBUF_SIZE 1024
+//uint64_t t_buf[TBUF_SIZE];
+//uint64_t tb_i = 0;
+uint64_t t_buf[MAX_THREADS][TBUF_SIZE];
+uint8_t rw_buf[MAX_THREADS][TBUF_SIZE];
+uint64_t timestamp_buf[MAX_THREADS][TBUF_SIZE];
+uint64_t tb_i[MAX_THREADS] = {};
+
+uint64_t numThreads = 0;
+
+
+// namespace IL1
+// {
+// // 1st level instruction cache: 32 kB, 32 B lines, 32-way associative
+// const UINT32 cacheSize                         = 32 * KILO;
+// const UINT32 lineSize                          = 32;
+// const UINT32 associativity                     = 32;
+// const CACHE_ALLOC::STORE_ALLOCATION allocation = CACHE_ALLOC::STORE_NO_ALLOCATE;
+
+// const UINT32 max_sets          = cacheSize / (lineSize * associativity);
+// const UINT32 max_associativity = associativity;
+
+// typedef CACHE_ROUND_ROBIN(max_sets, max_associativity, allocation) CACHE;
+// } // namespace IL1
+// //static IL1::CACHE il1("L1 Instruction Cache", IL1::cacheSize, IL1::lineSize, IL1::associativity);
+// //static IL1::CACHE *il1=new IL1::CACHE("L1 Instruction Cache", IL1::cacheSize, IL1::lineSize, IL1::associativity);
+// static IL1::CACHE *il1[MAX_THREADS];
+
+// namespace UL2
+// {
+// // 2nd level unified cache: 2 MB, 64 B lines, direct mapped
+// const UINT32 cacheSize                         = 2 * MEGA;
+// const UINT32 lineSize                          = 64;
+// const UINT32 associativity                     = 8;
+// const CACHE_ALLOC::STORE_ALLOCATION allocation = CACHE_ALLOC::STORE_ALLOCATE;
+
+// const UINT32 max_sets = cacheSize / (lineSize * associativity);
+
+// //typedef CACHE_DIRECT_MAPPED(max_sets, allocation) CACHE;
+// typedef CACHE_ROUND_ROBIN(max_sets, associativity, allocation) CACHE;
+// } // namespace UL2
+// static UL2::CACHE *ul2[MAX_THREADS];
+
+// namespace UL3
+// {
+// // 3rd level unified cache: 16 MB, 64 B lines, direct mapped
+// //const UINT32 cacheSize                         = 32 * MEGA;
+// const UINT32 cacheSize                         = 4 * MEGA; //per socket
+// const UINT32 lineSize                          = 64;
+// const UINT32 associativity                     = 8;
+// const CACHE_ALLOC::STORE_ALLOCATION allocation = CACHE_ALLOC::STORE_ALLOCATE;
+
+// const UINT32 max_sets = cacheSize / (lineSize * associativity);
+
+// //typedef CACHE_DIRECT_MAPPED(max_sets, allocation) CACHE;
+// typedef CACHE_ROUND_ROBIN(max_sets, associativity, allocation) CACHE;
+// } // namespace UL3
+// //static UL3::CACHE ul3("L3 Unified Cache", UL3::cacheSize, UL3::lineSize, UL3::associativity);
+// static UL3::CACHE *ul3[MAX_THREADS];
+
+
+
+static inline VOID dump_tbuf(THREADID tid) { //TODO add threaID to arg
+	if(tid>=MAX_THREADS){
+		return;
+	}
+    //added this as an optimization, but doesn't seem to save runtime much :(
+    uint64_t tmp_tbi = tb_i[tid];
+    for (uint64_t i = 0; i < tmp_tbi; i++) {
+        //original ver where trace was in text
+        //fprintf(trace[tid], "%p %d\n", (void*)(t_buf[tid][i]), rw_buf[tid][i]);
+        //fprintf(trace_sancheck, "%p %d\n", (void*)(t_buf[tid][i]), rw_buf[tid][i]);
+
+        //we don't care about last few bits of addr, so pack RW info in there
+        //uint64_t lsb_unsetter = ~0xF;
+        uint64_t addr_rw = t_buf[tid][i] & ~0xF;
+        addr_rw = addr_rw+rw_buf[tid][i];
+        uint64_t timestamp = timestamp_buf[tid][i];
+        fwrite(&addr_rw, sizeof(uint64_t), 1,trace[tid]);
+        fwrite(&timestamp, sizeof(uint64_t), 1,trace[tid]);
+    }
+    tb_i[tid] = 0;
+}
+
+static VOID Fini(int code, VOID* v) //TODO this should change to threadfini?
+{
+    //dump_tbuf();
+    //std::cout << "num mem accesses: "<<num_maccess <<std::endl;
+    std::cout << "Fini finished"<<std::endl;
+
+    FILE * doneIndicator = fopen("this_run_is_done.txt", "w");
+	fprintf(doneIndicator, "This run is done!");
+	fclose(doneIndicator);
+
+}
+VOID ThreadFini(THREADID tid, const CONTEXT* ctxt, INT32 code, VOID* v) {
+	if(tid>=MAX_THREADS){
+		return;
+	}
+
+    dump_tbuf(tid);
+    std::cout <<"thread_"<<tid << " num mem accesses: " << num_maccess[tid] << std::endl;
+    std::cout << "thread_" << tid << " Fini finished" << std::endl;
+    fclose(trace[tid]);
+    //fclose(ins_trace[tid]);
+}
+
+static inline VOID recordAccess(ADDRINT addr, THREADID tid, CACHE_BASE::ACCESS_TYPE accessType) { //TODO add threaID to arg
+	if(tid>=MAX_THREADS){
+		return;
+	}
+
+    num_maccess[tid]++;
+    t_buf[tid][tb_i[tid]] = addr;
+	rw_buf[tid][tb_i[tid]] = accessType;
+    timestamp_buf[tid][tb_i[tid]] = ins_count[tid];
+    tb_i[tid]++;
+    if (tb_i[tid] == TBUF_SIZE) {
+        dump_tbuf(tid);
+    }
+}
+
+// static inline VOID Ul3Access(ADDRINT addr, UINT32 size, CACHE_BASE::ACCESS_TYPE accessType, THREADID tid, bool dirtyEv_fromL2, bool isIns=false) ////TODO add threaID to arg
+// {
+// 	if(tid>=MAX_THREADS){
+// 		return;
+// 	}
+
+//     ADDRINT evicted_line=0;
+//     //const BOOL ul3hit = ul3[tid]->Access(addr, size, accessType);
+//     const BOOL ul3hit = ul3[tid]->Access(addr, size, accessType, evicted_line);
+//     if(!ul3hit){
+//         if(!dirtyEv_fromL2){ // if access is from dirtyEvict, it just installs the line from L2, no memory access
+//             recordAccess(addr, tid, CACHE_BASE::ACCESS_TYPE_LOAD); //access is read whether load or store. Write is only when dirty eviction
+//         }
+//         //if(isIns){ //skipping ins_trace for now
+//         //    fprintf(ins_trace[tid], "%p\n", (void*)(addr));
+//         //}
+//         if(evicted_line!=0){
+//             recordAccess(evicted_line, tid, CACHE_BASE::ACCESS_TYPE_STORE);
+//         }
+//     }
+// }
+
+// static VOID Ul2Access(ADDRINT addr, UINT32 size, CACHE_BASE::ACCESS_TYPE accessType, THREADID tid, bool isIns=false)
+// {
+// 	if(tid>=MAX_THREADS){
+// 		return;
+// 	}
+
+//     ADDRINT evicted_line=0;
+//     // second level unified cache
+//     const BOOL ul2Hit = ul2[tid]->Access(addr, size, accessType, evicted_line);
+
+//     // third level unified cache
+//     if(!ul2Hit) {
+//         Ul3Access(addr, size, accessType, tid, false, isIns);
+//         //writeback (to next level, L3)
+//         if(evicted_line!=0){
+//             Ul3Access(evicted_line, 1, CACHE_BASE::ACCESS_TYPE_STORE, tid, true, false);
+//         }
+//     }
+//}
+
+
+static VOID InsRef(ADDRINT addr, THREADID tid) //TODO add threaID to arg
+{
+	if(tid>=MAX_THREADS){
+		return;
+	}
+
+	BOOL inROI_check = (inROI[tid]) || (inROI_master);
+    if(!inROI_check){
+    //if(!inROI[tid]){
+        return;
+    }
+
+
+    if(ins_count[tid] > 40000000000){
+		std::cout<<"killing after 40B instructions"<<std::endl;
+		exit(0);
+	}
+
+    ins_count[tid]++;
+    if(ins_count[tid] % 100000000==0){
+    //if(ins_count[tid] % 1000==0){ // for testing
+        if(ins_count[tid] % 1000000000==0){
+            std::cout<<"thread_"<<tid << " ins_count: " << ins_count[tid] / 1000000000 << " B" << std::endl;
+        }
+        if(ins_count[tid] >= MT_END){
+            tr_done[tid]=true;
+            uint64_t done_count=0;
+            for (uint64_t ii=0;ii<MAX_THREADS;ii++){
+                if(tr_done[ii]) done_count++;
+                else break;
+            }
+            if(done_count==MAX_THREADS){
+                std::cout<<"all threads tracing done"<<std::endl;
+                exit(0);
+            }
+        }
+        //Mark timestamp in the trace file
+        dump_tbuf(tid);
+        //fprintf(trace[tid], "INST_COUNT %ld\n", ins_count[tid]);
+        uint64_t ts_signature=0xc0ffee;
+        fwrite(&ts_signature, sizeof(uint64_t), 1,trace[tid]); //signature for inst count
+        uint64_t tmp_inscount =ins_count[tid];
+        fwrite(  &tmp_inscount, sizeof(uint64_t), 1,trace[tid]);
+
+        //instrace write
+        //fprintf(ins_trace[tid], "INST_COUNT %ld\n", ins_count[tid]);
+    }
+
+}
+
+static VOID MemRefMulti(ADDRINT addr, UINT32 size, CACHE_BASE::ACCESS_TYPE accessType, THREADID tid) //TODO add threaID to arg
+{
+	if(tid>=MAX_THREADS){
+		return;
+	}
+
+	BOOL inROI_check = (inROI[tid]) || (inROI_master);
+    if(!inROI_check){
+    //if(!inROI[tid]){
+        return;
+    }
+    if(ins_count[tid]<MT_START) return;
+    if(ins_count[tid]>MT_END) return;
+
+    recordAccess(addr, tid, accessType);
+    //TODO figure out how to handle memref_multi
+    //Ul3Access(addr, size, accessType, tid);
+    //Ul2Access(addr, size, accessType, tid);
+    return;
+
+}
+
+static VOID MemRefSingle(ADDRINT addr, UINT32 size, CACHE_BASE::ACCESS_TYPE accessType, THREADID tid) //TODO add threaID to arg
+{
+	if(tid>=MAX_THREADS){
+		return;
+	}
+
+	BOOL inROI_check = (inROI[tid]) || (inROI_master);
+    if(!inROI_check){
+        return;
+    }
+    if(ins_count[tid]<MT_START) return;
+    if(ins_count[tid]>MT_END) return;
+
+    recordAccess(addr, tid, accessType);
+    //Ul3Access(addr, size, accessType, tid);
+    //Ul2Access(addr, size, accessType, tid);
+    return;
+    //DBG counter
+    //memref_single_count++;
+    //if(memref_single_count % 10000==0) {std::cout<<"memref_single_count : "<<memref_single_count/10000<<"*10k"<<std::endl;}
+    //DBG for all accesses
+    //fprintf(trace, "%p\n", (void *)addr);
+    //return;
+
+
+}
+
+static VOID pin_magic_inst(THREADID tid, ADDRINT value, ADDRINT field){
+	if(tid>=MAX_THREADS){
+		return;
+	}
+
+        switch(field){
+            case 0x0: //ROI START
+                inROI[tid]=true;
+                std::cout<<"ROI START (tid "<<tid<<")"<<std::endl;
+                break;
+            case 0x2: //ROI START_master
+				inROI_master=true;
+                std::cout<<"ROI START (MASTER)"<<std::endl;
+                break;
+            case 0x3: //ROI START (same as 0 but don't print anything
+                inROI[tid]=true;
+                //std::cout<<"ROI START (tid "<<tid<<")"<<std::endl;
+                break;
+            case 0x4: //ROI END
+                inROI[tid]=false;
+                //std::cout<<"ROI START (tid "<<tid<<")"<<std::endl;
+                break;
+            case 0x5: //Print Instruction count (dbg, setting FF, etc)
+                std::cout<<"TRACER: MI-5 - Ins Count: "<<ins_count[tid]<<std::endl;
+                break;
+            default:
+                break;
+
+        }
+        return;
+}
+
+static VOID Instruction(INS ins, VOID* v)
+{
+    //THREADID curtid = PIN_ThreadId();
+    // all instruction fetches access I-cache
+    INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)InsRef, IARG_INST_PTR, IARG_THREAD_ID, IARG_END);
+
+    // MAGIC INSTRUCTIONS
+    // start ROI if started 'FF'
+    if (INS_IsXchg(ins) && INS_OperandReg(ins, 0) == REG_RBX && INS_OperandReg(ins, 1) == REG_RBX) {
+        //std::cout<<"(xchg rbx rbx caught)!"<<std::endl;
+        INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)pin_magic_inst, IARG_THREAD_ID, IARG_REG_VALUE, REG_RBX, IARG_REG_VALUE, REG_RCX, IARG_END);
+        //INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)ROI_start, IARG_THREAD_ID, IARG_END);
+    }
+
+
+    if (!INS_IsStandardMemop(ins)) return;
+    if (INS_MemoryOperandCount(ins) == 0) return;
+    ;
+
+    UINT32 readSize = 0, writeSize = 0;
+    UINT32 readOperandCount = 0, writeOperandCount = 0;
+
+    for (UINT32 opIdx = 0; opIdx < INS_MemoryOperandCount(ins); opIdx++)
+    {
+        if (INS_MemoryOperandIsRead(ins, opIdx))
+        {
+            readSize = INS_MemoryOperandSize(ins, opIdx);
+            readOperandCount++;
+            break;
+        }
+        if (INS_MemoryOperandIsWritten(ins, opIdx))
+        {
+            writeSize = INS_MemoryOperandSize(ins, opIdx);
+            writeOperandCount++;
+            break;
+        }
+    }
+
+    if (readOperandCount > 0)
+    {
+        const AFUNPTR countFun = (readSize <= 4 ? (AFUNPTR)MemRefSingle : (AFUNPTR)MemRefMulti);
+
+        // only predicated-on memory instructions access D-cache
+        INS_InsertPredicatedCall(ins, IPOINT_BEFORE, countFun, IARG_MEMORYREAD_EA, IARG_MEMORYREAD_SIZE, IARG_UINT32,
+                                 CACHE_BASE::ACCESS_TYPE_LOAD, IARG_THREAD_ID, IARG_END);
+    }
+
+    if (writeOperandCount > 0)
+    {
+        const AFUNPTR countFun = (writeSize <= 4 ? (AFUNPTR)MemRefSingle : (AFUNPTR)MemRefMulti);
+
+        // only predicated-on memory instructions access D-cache
+        INS_InsertPredicatedCall(ins, IPOINT_BEFORE, countFun, IARG_MEMORYWRITE_EA, IARG_MEMORYWRITE_SIZE, IARG_UINT32,
+                                 CACHE_BASE::ACCESS_TYPE_STORE, IARG_THREAD_ID, IARG_END);
+    }
+}
+
+VOID ThreadStart(THREADID tid, CONTEXT* ctxt, INT32 flags, VOID* v) {
+	if(tid>=MAX_THREADS){
+        std::cout<<"threadid larger than "<<MAX_THREADS<<std::endl;
+		return;
+	}
+
+    std::cout << "thread_" << tid<< " start" << std::endl;
+    numThreads++;
+    //std::string tfname = "memtrace_t" + std::to_string(tid) + ".out";
+    std::ostringstream tfname;
+    tfname << "memtrace_t" << tid << ".out";
+    trace[tid] = fopen(tfname.str().c_str(), "wb");
+    //trace_sancheck = fopen("asdf.txt", "w");
+
+
+    if(KnobStartFF){
+        inROI[tid]=false;
+    }
+    else{
+        inROI[tid]=true;
+    }
+
+
+    // il1[tid] = new IL1::CACHE("L1 Instruction Cache", IL1::cacheSize, IL1::lineSize, IL1::associativity);
+    // ul2[tid] = new UL2::CACHE("L2 Unified Cache", UL2::cacheSize, UL2::lineSize, UL2::associativity);
+    // ul3[tid] = new UL3::CACHE("L3 Unified Cache", UL3::cacheSize, UL3::lineSize, UL3::associativity);
+}
+
+extern int main(int argc, char* argv[])
+{
+    PIN_Init(argc, argv);
+
+    if(KnobStartFF){
+		std::cout<<"startFF"<<std::endl;
+		inROI_master=false;
+	}
+	else{
+		inROI_master=true;
+	}
+    champsim_skipins  = KnobSkipInstructions.Value();
+    champsim_traceins = KnobTraceInstructions.Value();
+    champsim_tracedoneins = champsim_skipins+champsim_traceins;
+    //std::cout<<"champsim skipins "<<champsim_skipins<<", champsim traceins: "<<champsim_traceins<<", champsim_tracedoneins: "<<champsim_tracedoneins<<std::endl;
+    std::cout << "champsim traceins: " << champsim_traceins << std::endl;
+
+    if(KnobTraceInstructions.Value() > PHASE_INTERVAL){
+        std::cout << "Traced insts cannot be larger than PHASE_INTERVAL" << std::endl;
+        std::cout << "PHASE_INTERVAL: " << PHASE_INTERVAL << std::endl;
+        std::cout << "Trace instructions: " << KnobTraceInstructions.Value() << std::endl;
+        exit(1);
+    }
+
+
+
+    //trace = fopen(KnobOutputFile.Value().c_str(), "w");
+    INS_AddInstrumentFunction(Instruction, 0);
+    PIN_AddFiniFunction(Fini, 0);
+
+    PIN_AddThreadStartFunction(ThreadStart, 0);
+    PIN_AddThreadFiniFunction(ThreadFini, 0);
+
+    std::cout<<"before calling prog"<<std::endl;
+    // Never returns
+    PIN_StartProgram();
+    std::cout<<"never returns"<<std::endl;
+    return 0; // make compiler happy
+}

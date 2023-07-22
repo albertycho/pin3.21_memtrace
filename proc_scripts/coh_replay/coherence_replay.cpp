d99ad1a3624c87994573162135229dcd95c402c9
Albert Y Cho <albertycho90@gmail.com>
2023-07-22 00:03:26 -0400

    debugged versions. not cleanedup

--- proc_scripts/coh_replay/coherence_replay.cpp
+++ proc_scripts/coh_replay/coherence_replay.cpp
@@ -35,23 +35,9 @@ int main(){
     assert(isPowerOfTwo(NUM_SETS));
     syscycle=0;
 
-    CCD[200][150]={DState::I,10,{0}};
-    std::cout<<CCD[200][150].owner<<endl;
-    
     init_caches();
 
-    access_cache(caches[4], 0x7f0124124, true, 8274,0);
-    std::cout<<caches[4].entries[292][23].ts<<endl;
-    access_cache(caches[4], 0x7f0124124, true, 8278,1);
-    access_cache(caches[4], 0x7f0124124+(NUM_SETS*64), true, 8362,2);
-    std::cout<<caches[4].entries[292][23].ts<<endl;
-    std::cout<<caches[4].entries[292][22].ts<<endl;
-    std::cout<<"direcotry[292] size: "<<CCD[292].size()<<endl;
-    std::cout<<"direcotry[295] size: "<<CCD[295].size()<<endl;
-
-    log_cur_stats();
-
-    return 0; //for dbg
+    //return 0; //for dbg
     //open traces
     for(int i=0; i<N_THR;i++){
 		std::ostringstream tfname;
@@ -71,11 +57,14 @@ int main(){
             char buffer[8];
 		    uint64_t buf_val=0;
             size_t readsize = read_8B_line(&buf_val, buffer, trace[i]);
+            if(readsize!=8){
+                cout<<"Trace Readline Failed!"<<endl;
+            }
             if(buf_val==0xc0ffee){ // 1B inst phase done
 				read_8B_line(&buf_val, buffer, trace[i]);
                 if((buf_val>=TRACESTARTINST) && (i==2)){
                     cout<<"timestampline : "<<buf_val<<endl;
-                    log_cur_stats();
+                    log_cur_stats(buf_val);
                 }
                 if(buf_val==6000000000){
                     ts_6Bhit=true;
@@ -96,9 +85,16 @@ int main(){
         if(ts_6Bhit) break;
 
     }
+    //print touched memory:
+    uint64_t touched_mem = 0;
+    for(int jj=0;jj<N_SOCKETS;jj++){
+        touched_mem+=CCD[jj].size();
+    }
+    touched_mem = touched_mem * 64;
+    cout<<"Touched Mem: ";
+    if(touched_mem > 1000000000){cout<<touched_mem/1000000000<<"GB"<<endl;}
+    else{cout<<touched_mem/1000000<<"MB"<<endl;}
 }
-
-
 
 
 
@@ -112,4 +108,5 @@ int main(){
     std::cout<<"direcotry[292] size: "<<CCD[292].size()<<endl;
     std::cout<<"direcotry[295] size: "<<CCD[295].size()<<endl;
 
+    log_cur_stats();
 */

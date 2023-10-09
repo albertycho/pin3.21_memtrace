31c4b6791b07651bf6bd79c463edd1416e524c1d
Albert <albertycho90@gmail.com>
2023-10-09 18:04:49 -0400

    smarthop plotting update to reflect coherence block transfers

--- proc_scripts/plot_smart_hops.py
+++ proc_scripts/plot_smart_hops.py
@@ -3,7 +3,7 @@ from matplotlib.patches import Patch
 import csv
 import numpy as np
 
-lats=[80,130,360,180]
+lats=[80,130,360,180,413,280]
 
 # Load the data
 data = []
@@ -15,8 +15,8 @@ with open('collected_hop_stats.txt', 'r') as file:
 benchmarks = [row[0] for row in data]
 
 # Prepare the data for plotting
-base_values = [[int(val) for val in row[1:5]] for row in data]
-cxi_values = [[int(val) for val in row[5:9]] for row in data]
+base_values = [[int(val) for val in row[1:7]] for row in data]
+cxi_values = [[int(val) for val in row[7:13]] for row in data]
 
 # Normalize the counts to PDFs
 base_values = [[val/sum(group) for val in group] for group in base_values]
@@ -29,7 +29,8 @@ indices = np.arange(n_benchmarks)
 
 # Colors for the bars
 #colors = ['tab:green', 'tab:blue', 'tab:red', 'tab:purple']
-colors=['#dda0dd', '#8fbc8f', '#faa460', '#9370db']
+#colors=['#dda0dd', '#8fbc8f', '#faa460', '#9370db']
+colors=['#dda0dd', '#8fbc8f', '#faa460', '#9370db','lightyellow','aliceblue']
 plt.rcParams.update({'font.size': 14})
 labelfontsize=20
 # Create the figure and the axes
@@ -75,19 +76,19 @@ print(ctwohoprat_mean)
 bottom_values = [0] * len(base_values)
 for i, color in enumerate(colors):
     plotvals = [base[i] for base in base_values]
-    ax1.bar(indices - bar_width * 1.25, plotvals, bar_width, color=color, alpha=1, bottom=bottom_values)
+    ax1.bar(indices - bar_width * 1.25, plotvals, bar_width, color=color, alpha=1, bottom=bottom_values, edgecolor='black')
     bottom_values = [a + b for a, b in zip(plotvals, bottom_values)]
 
 # Plot cxi bars
 cbottom_values = [0] * len(cxi_values)
 for i, color in enumerate(colors):
     plotvals = [cxi[i] for cxi in cxi_values]
-    ax1.bar(indices + bar_width * 1.25, plotvals, bar_width, color=color, hatch='//', alpha=1, bottom=cbottom_values)
+    ax1.bar(indices + bar_width * 1.25, plotvals, bar_width, color=color, hatch='//', alpha=1, bottom=cbottom_values, edgecolor='black')
     cbottom_values = [a + b for a, b in zip(plotvals, cbottom_values)]
 
 # Plot amat bars
-ax2.bar(indices - bar_width * 1.25, base_amats, bar_width * 0.8, color='#ce4257', alpha=1)
-ax2.bar(indices + bar_width * 1.25, cxi_amats, bar_width * 0.8, color='#ce4257', hatch='//', alpha=0.8)
+ax2.bar(indices - bar_width * 1.25, base_amats, bar_width * 0.8, color='#ce4257', alpha=1,edgecolor='black')
+ax2.bar(indices + bar_width * 1.25, cxi_amats, bar_width * 0.8, color='#ce4257', hatch='//', alpha=0.8, edgecolor='black')
 
 # Adjust layout to prevent overlap
 plt.tight_layout()
@@ -110,15 +111,19 @@ ax2.set_ylabel('Estimated\nAMAT (ns)', fontsize=labelfontsize)
 ax2.set_xticklabels(benchmarks, rotation=0)
 #ax2.set_ylabel('Average Memory \nAccess Time (ns)', fontsize=labelfontsize, labelpad=10)
 # Create legend
-legend_elements = [Patch(facecolor=color, label=headers[i+1].split('_')[-1]) for i, color in enumerate(colors)]
+legend_elements = [Patch(facecolor=color, label=headers[i+1].split('_')[-1], edgecolor='black') for i, color in enumerate(colors)]
 #legend_elements[3].label='CXL'
-legend_elements[3] = Patch(facecolor=legend_elements[3].get_facecolor(), label='Pool')
-legend_elements[0] = Patch(facecolor=legend_elements[0].get_facecolor(), label='Local')
+legend_elements[3] = Patch(facecolor=legend_elements[3].get_facecolor(), label='Pool', edgecolor='black')
+legend_elements[0] = Patch(facecolor=legend_elements[0].get_facecolor(), label='Local', edgecolor='black')
+legend_elements[4] = Patch(facecolor=legend_elements[4].get_facecolor(), label='BT_NoPool', edgecolor='black')
+legend_elements[5] = Patch(facecolor=legend_elements[5].get_facecolor(), label='BT_Pool', edgecolor='black')
 #legend_elements.append(Patch(facecolor='white', alpha=0, label=''))
-legend_elements.append(Patch(facecolor='grey', label='Baseline'))
-legend_elements.append(Patch(facecolor='grey', label='StarNUMA', hatch='//'))
+legend_elements.append(Patch(facecolor='grey', label='Baseline',edgecolor='black'))
+legend_elements.append(Patch(facecolor='grey', label='StarNUMA', hatch='//',edgecolor='black'))
 
-ax1.legend(handles=legend_elements, loc='upper right', ncol=3)
+#ax1.legend(handles=legend_elements, loc='upper right', ncol=3)
+#ax1.legend(handles=legend_elements, loc='lower right', ncol=4)
+ax1.legend(handles=legend_elements, ncol=4, loc='center', bbox_to_anchor=(0.5,1.15))
 legend_elements.append(Patch(facecolor='white', alpha=0, label=''))
 legend_elements.append(Patch(facecolor='black', label='Baseline'))
 legend_elements.append(Patch(facecolor='grey', label='StarNUMA', hatch='//'))
